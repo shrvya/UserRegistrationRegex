79bba272cdf4359fc4a1a2d3fd4bd8e35e75ed7b
shrvya <shrivyashetty@gmail.com>
2021-09-07 14:52:30 +0530

    added:User registration

---
+++ regularexpression/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.regularexpression.regularexpression;
+
+public class UserRegistration {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to User Registration");
+	}
+
+}
